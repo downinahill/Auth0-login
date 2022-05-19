421ad69558000d4860e4e745bcbe2e61224e6f81
Bfrisbyh92 <Bfrisbyh92@gmail.com>
2022-05-19 18:33:27 -0400

    Set up Auth0 without anything customized

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { useAuth0 } from '@auth0/auth0-react';
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
+import User from './components/User';
+
+
 
 function App() {
+  const clientId = process.env.REACT_APP_CLIENT_ID
+  console.log(clientId);
+  const { isAuthenticated } = useAuth0();
+  console.log({isAuthenticated})
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!isAuthenticated ? (
+        <div>
+          <p style={{ fontSize: "1.5rem" }}>Please Login.</p>
+           <LoginButton />
+        </div>
+      ) :
+        <div>
+            <LogoutButton />
+            <User />
+        </div>}
     </div>
   );
 }
 
 export default App;
+
+
