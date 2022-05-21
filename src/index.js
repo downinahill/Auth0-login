c147fceff94787346cd4dc10051546e13043550a
downinahill <brian.hill.0023@gmail.com>
2022-05-21 12:39:56 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import { Auth0Provider } from "@auth0/auth0-react";
 
-ReactDOM.render(
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement)
+const domain = process.env.REACT_APP_DOMAIN
+const clientId = process.env.REACT_APP_CLIENT_ID
+
+root.render(
+  <React.StrictMode>
   <Auth0Provider
-    domain="dev-lhmvhvni.us.auth0.com"
-    clientId="BwYOxV0vRoviTd9gS1wJueRa8GfxRAmY"
+    domain={domain}
+    clientId={clientId}
     redirectUri={window.location.origin}
   >
     <App />
-  </Auth0Provider>,
-  document.getElementById("root")
+  </Auth0Provider>
+  </React.StrictMode>
+  
 );
